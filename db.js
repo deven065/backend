7d42505929336cf3884123a484d177d43d273ee7
Deven065 <devenrikame55@gmail.com>
2025-04-20 22:59:34 +0530

    Backend Deployment processing

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+require ("dotenv").config();
 
-mongoose.connect("mongodb://localhost:27017/paytm")
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log("MongoDB connected"))
     .catch(err => console.error("MongoDB connection error:", err));
 
--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const rootRouter = require('./routes/index');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin : "https://paytm-clone-frontend.vercel.app", //replace after frontend deployment
+    methods : ["GET", "POST", "PUT", "DELETE"],
+    credentials : true
+}));
 app.use(express.json());
 
 app.use("/api/v1", rootRouter);
