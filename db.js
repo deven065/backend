21eacce1fe26294e06774b69bc232bc0809782ea
Deven065 <devenrikame55@gmail.com>
2025-04-22 16:46:52 +0530

    database updated

--- db.js
+++ db.js
@@ -1,14 +1,7 @@
 const mongoose = require("mongoose");
 require ("dotenv").config();
 
-mongoose.connect(process.env.MONGODB_URL)
-  .then(() => {
-    console.log("Connected to MongoDB");
-  })
-  .catch((err) => {
-    console.error("Error connecting to MongoDB:", err);
-  });
-
+mongoose.connect("mongodb://localhost:27017/paytm");
 
 // Create a schema for user
 const userSchema = new mongoose.Schema({
