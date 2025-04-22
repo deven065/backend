79dd25d8e072fa8765c37c307b49791a13dc42c8
Deven065 <devenrikame55@gmail.com>
2025-04-23 02:37:38 +0530

    backend re-worked

--- index.js
+++ index.js
@@ -4,11 +4,7 @@ const rootRouter = require('./routes/index');
 
 const app = express();
 
-app.use(cors({
-    origin : "https://paytm-clone-frontend.vercel.app", //replace after frontend deployment
-    methods : ["GET", "POST", "PUT", "DELETE"],
-    credentials : true
-}));
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/v1", rootRouter);
